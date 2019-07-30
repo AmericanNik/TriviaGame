d2cc202df04e8dc958996b6abc1f2ac118c79b97
AmericanNik <49841405+AmericanNik@users.noreply.github.com>
2019-07-30 04:15:08 -0400

    facelift & added questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,78 +1,171 @@
 $(document).ready(function() {
-
   console.log('ready');
-  //hooks into document
-  let time = 30;
-  let docTime = $('#time');
-  let docQuestion = $('#question');
-  let docChoices = Array.from($('.choice-text'));
-  let selectedQuestion = {};
-  let userAnswer = true;
-  let score = 0;
-  let gameCounter = 0;
-  let questionBank = [{
-    question: "Inside which HTML element do we put the JavaScript??",
-    choice1: "<script>",
-    choice2: "<javascript>",
-    choice3: "<js>",
-    choice4: "<scripting>",
-    answer: 1
-  }, {
-    question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-    choice1: "<script href='xxx.js'>",
-    choice2: "<script name='xxx.js'>",
-    choice3: "<script src='xxx.js'>",
-    choice4: "<script file='xxx.js'>",
-    answer: 3
-  }, {
-    question: " How do you write 'Hello World' in an alert box?",
-    choice1: "msgBox('Hello World');",
-    choice2: "alertBox('Hello World');",
-    choice3: "msg('Hello World');",
-    choice4: "alert('Hello World');",
-    answer: 4
-  }];
 
-  console.log(docChoices)
+const question = $('#question');
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+let docCorrect = $('#correct');
+let docIncorrect = $('#incorrect');
+let docQcorrect = $('#correctQuestion');
+let docQincorrect = $('#incorrectQuestion');
+let docAcorrect = $('#correctAnswer');
+let docAincorrect = $('#incorrectAnswer');
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuesions = [];
+let docTime = $('#time');
+let time = 30;
+let correct = 0;
+let incorrect = 0;
 
 
+let questions = [
+{
+  question: "What year did the 13 Colonies declare indepdence from England?",
+  choice1: "1776",
+  choice2: "1841",
+  choice3: "1727",
+  choice4: "1813",
+  answer: 1
+},
+{
+  question:
+    "Who was the first President of The United States of America?",
+  choice1: "Thomas Jefferson",
+  choice2: "Abraham Lincoln",
+  choice3: "George Washington",
+  choice4: "John Adams",
+  answer: 3
+},
+{
+  question: "When was the Constitution signed?",
+  choice1: "4th July, 1776",
+  choice2: "21st Febuary, 1812 ",
+  choice3: "31st March, 1803",
+  choice4: "21 June, 1788",
+  answer: 4
+},
+{
+  question: "What is the 1st amendment?",
+  choice1: "Right to Privacy",
+  choice2: "Freedom of Speech",
+  choice3: "Women's sufferage",
+  choice4: "Abolishes Slavery",
+  answer: 2
+},
+{
+  question: "When was the Constitution signed?",
+  choice1: "4th July, 1776",
+  choice2: "21st Febuary, 1812 ",
+  choice3: "31st March, 1803",
+  choice4: "21 June, 1788",
+  answer: 4
+},
+{
+  question: "How many states succeeded from the United States during the Civil War?",
+  choice1: "22 States",
+  choice2: "15 States",
+  choice3: "8 States",
+  choice4: "11 States",
+  answer: 4
+},
+{
+  question: "What is the 2nd Amendment?",
+  choice1: "Right to bear arms",
+  choice2: "Freedom of the Press",
+  choice3: "Freedom to Practice Any Religion",
+  choice4: "Pursuit of happiness",
+  answer: 1
+},
+{
+  question: "What are the 3 branches of government?",
+  choice1: "Republicans, Democrats, Indepdents",
+  choice2: "Democrats, Republicans, Executive",
+  choice3: "The Senate, the House of Representatives, and the President",
+  choice4: "Executive, Legislative, Judicial",
+  answer: 4
+}
+];
 
-  gameStart = function(){
-    questionCounter = 0;
-    score = 0;
-    availableQuestions = questionBank;
-    console.log(availableQuestions);
+//CONSTANTS
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 5;
+
+function timeout() {
+  setTimeout(function() {
+    time--;
+    console.log('tick');
+    docTime.text(time);
+    docCorrect.text(correct);
+    docIncorrect.text(incorrect);
+
+    if (time === 0) {
+      return false;
+    }
+
     timeout();
-    loadQuestion();
-  };
+  }, 1000);
+}
 
-  loadQuestion= function(){
-    questionCounter++;
-    let questionIndex = Math.floor(Math.random()*availableQuestions.length);
-    selectedQuestion = availableQuestions[questionIndex];
-    docQuestion.text(selectedQuestion.question);
-    docChoices.forEach( function(){
-      const number = choice.dataset['number'];
-      docChoices.text(selectedQuestion['choice' + number]);
-    });
-  };
-  // Countdown
+startGame = () => {
+timeout();
+questionCounter = 0;
+score = 0;
+availableQuesions = [...questions];
+console.log(availableQuesions);
+getNewQuestion();
+};
 
-  function timeout() {
-    setTimeout(function() {
-      time--;
-      console.log('tick');
-      docTime.text(time);
+getNewQuestion = () => {
+if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+  question.text('END GAME!')
+  //go to the end page
+  // return window.location.assign("/end.html");
+}
+questionCounter++;
+const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+currentQuestion = availableQuesions[questionIndex];
+question.text(currentQuestion.question);
 
-      if (time === 0) {
-        return false;
-      }
+choices.forEach(choice => {
+  const number = choice.dataset["number"];
+  choice.innerText = currentQuestion["choice" + number];
+});
 
-      timeout();
-    }, 1000);
-  }
+availableQuesions.splice(questionIndex, 1);
+console.log(availableQuesions);
+acceptingAnswers = true;
+};
 
-  gameStart();
+choices.forEach(choice => {
+choice.addEventListener("click", e => {
+  if (!acceptingAnswers) return;
+
+  acceptingAnswers = false;
+  const selectedChoice = e.target;
+  const selectedAnswer = selectedChoice.dataset["number"];
+  console.log('selected answer '+selectedAnswer);
+  console.log('Question Counter '+ questionCounter)
+  console.log(selectedAnswer == currentQuestion.answer)
+    if (selectedAnswer == currentQuestion.answer){
+      correct++;
+      docQcorrect.prepend("currentQuestion");
+      docAcorrect.prepend(currentQuestion.answer);
+    }else{
+      incorrect++
+    }
+  getNewQuestion();
+});
+});
+
+startGame();
+
+
+
+
 
   // api connect
   // $.ajax({
